f951d5c394b6c05e7c803f78ff343eb546f0ff60
JoaoMNZ <joao.menezes21@Outlook.com>
2026-03-04 12:06:16 -0300

    Setup test environment.

--- backend/src/test/java/io/github/joaomnz/bettracker/BettrackerApplicationTests.java
+++ backend/src/test/java/io/github/joaomnz/bettracker/BettrackerApplicationTests.java
@@ -2,8 +2,10 @@ package io.github.joaomnz.bettracker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BettrackerApplicationTests {
 
 	@Test
